0f40d868701e4febfee0af2c3c99d3f228439dd9
ryanflowers-developer <51009900+ryanflowers-developer@users.noreply.github.com>
2020-07-22 16:45:38 -0400

    third test

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -4,6 +4,7 @@
         <v-col class="mx-auto my-auto" cols="6">
           <h1>Test</h1>
           <h2>Test 2</h2>
+          <h3>Test 3</h3>
           <v-img src="https://picsum.photos/800/500"></v-img>
         </v-col>
       </v-row>
