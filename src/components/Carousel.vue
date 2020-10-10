598380a3921afc4a2bf9f89594eaa90a7ce5d37c
Ryan Flowers <hexstone@Ryans-MacBook-Pro.local>
2020-10-10 11:30:46 -0400

    Adjusted carousel

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -8,7 +8,7 @@
       
 
       </div>
-    <v-carousel height="450px" cycle hide-delimiter-background show-arrows-on-hover>
+    <v-carousel height="500px" cycle hide-delimiter-background show-arrows-on-hover>
       <v-carousel-item v-for="image in images" :key="image">
         <v-img :src="image"></v-img>
       </v-carousel-item>
